b637104d804082233b34f0a53cc043a718bc6c4a
Athanas Muinde <athanasmwandi5@gmail.com>
2025-07-13 21:45:30 +0300

    added new js file

---
+++ 7-multi_c.js
@@ -0,0 +1,8 @@
+const args = process.argv.slice(2);
+const count = Number(args[0]);
+
+if (!Number.isInteger(count)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < count; i++) console.log("C is fun");
+}
